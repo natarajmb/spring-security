e4f97f8ad62f2c1de808d7f36ad2ea75c4b1604a
Nataraj Basappa <nataraj.basappa@yahoo.com>
2014-07-29 18:02:23 +0100

    Added a redirection to authenticated page if the user is logged in

--- src/main/java/com/cognitivenode/security/controller/PageController.java
+++ src/main/java/com/cognitivenode/security/controller/PageController.java
@@ -8,6 +8,9 @@
 
 package com.cognitivenode.security.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +33,11 @@ public class PageController {
     @RequestMapping(value = "/login.do")
     public String getLoginPage(Model model) {
         System.out.println("login");
-        return "login";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication instanceof AnonymousAuthenticationToken)
+            return "login";
+        else
+            return "redirect:/secure/authenticated.do";
     }
 
     @RequestMapping(value = "/secure/authenticated.do")
